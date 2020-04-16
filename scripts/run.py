e4af9177095e60e817f49bfe88efa594ab26e8a8
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2020-04-16 17:56:37 +0530

    script update

--- scripts/run.py
+++ scripts/run.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import errno
+import json
 import os
 import shutil
 import sys
@@ -7,10 +9,12 @@ import urllib.request
 import zipfile
 import platform
 import subprocess
+from distutils.dir_util import copy_tree
 
 logging.basicConfig(filename="debug.log", level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler())
 rootPath = os.path.abspath(os.path.join(os.path.realpath(__file__), "../.."))
+tmpPath = os.path.join(rootPath, ".tmp")
 parser = argparse.ArgumentParser(description='Script to setup testrig')
 logging.info("root path: " + rootPath)
 # General Arguments
@@ -18,22 +22,51 @@ subparsers = parser.add_subparsers(dest='mode', help='sub-command help')
 subparsers.required = True
 subparsers_setup = subparsers.add_parser('setup', help='Setup testrig')
 subparsers_run = subparsers.add_parser('run', help='Run testrig')
+subparsers_test = subparsers.add_parser('test', help='Run test')
 args = parser.parse_args()
 print(args)
+print("root_path: "+rootPath)
+print("tmp_path: "+tmpPath)
 # global variables #
-partnerDemoServiceUrl = "https://mosip.s3-us-west-2.amazonaws.com/1.0.6-rc/authentication-partnerdemo-service.jar"
+partnerDemoServiceUrl = "https://mosip.s3-us-west-2.amazonaws.com/authentication-partnerdemo-service.jar"
 partnerDemoServiceJarPath = os.path.join(rootPath, "ivv-orchestrator/dependency_jars/authentication-partnerdemo-service.jar")
 
-registrationServicesUrl = "https://mosip.s3-us-west-2.amazonaws.com/1.0.6-rc/registration-services-1.0.6-rc.jar"
-registrationServicesJarPath = os.path.join(rootPath, "ivv-registration/services_jar/registration-services-1.0.6-rc.jar")
+registrationClientUrl = "http://13.71.87.138:8040/artifactory/libs-release-local/io/mosip/registration/registration-client/1.0.7/mosip-sw-1.0.7.zip"
+registrationClientZipPath = os.path.join(tmpPath, "rc.zip")
 
-identySDKUrl = "https://mosip.s3-us-west-2.amazonaws.com/1.0.6-rc/identy-sdk.jar"
+identySDKUrl = "https://mosip.s3-us-west-2.amazonaws.com/identy-sdk.jar"
 identySDKJarPath = os.path.join(rootPath, "ivv-registration/services_jar/identy-sdk.jar")
 
-databaseUrl = "https://mosip.s3-us-west-2.amazonaws.com/1.0.6-rc/db.zip"
-databaseLocalZipPath = os.path.join(rootPath, "ivv-orchestrator/db.zip")
-databaseLocalFolderPath = os.path.join(rootPath, "ivv-orchestrator")
 
+environment = ""
+emailServerHost = ""
+emailServerUsername = ""
+emailServerPassword = ""
+
+def preSetup():
+    if not os.path.exists(tmpPath):
+        os.makedirs(tmpPath)
+    preRun()
+
+def preRun():
+    properties_path = os.path.join(os.path.realpath(__file__), './../properties.json')
+    if os.path.isfile(properties_path):
+        with open(properties_path, 'r') as file:
+            properties = json.loads(file.read())
+            registrationClientUrl = properties['registrationClientUrl']
+            environment = properties['environment']
+            emailServerHost = properties['emailServerHost']
+            emailServerUsername = properties['emailServerUsername']
+            emailServerPassword = properties['emailServerPassword']
+            logging.info("Properties :")
+            logging.info("registrationClientUrl -> "+registrationClientUrl)
+            logging.info("environment -> "+environment)
+            logging.info("emailServerHost -> "+emailServerHost)
+            logging.info("emailServerUsername -> "+emailServerUsername)
+            logging.info("emailServerPassword -> "+emailServerPassword)
+            user_input = input("Hello contributor, Check the properties carefully. Then press any key to continue...")
+    else:
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), properties_path)
 
 def checkPlatform():
     if platform.system() != 'Linux' and  platform.system() != 'Windows':
@@ -45,7 +78,7 @@ def checkPlatform():
 def checkMaven():
     logging.info("Checking maven")
     try:
-        ds = subprocess.run(["mvn", "-v"], shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ds = subprocess.run(["mvn", "-v"], shell = True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if ds.returncode != 0:
             logging.error("Failed to check maven version")
             return
@@ -53,17 +86,14 @@ def checkMaven():
         if ds.stdout is None:
             logging.error("Maven not installed")
             return
-    except subprocess.CalledProcessError:
-        logging.error(subprocess.CalledProcessError.stderr)
+    except subprocess.CalledProcessError as e:
+        logging.error(e.output)
 
 
 def checkJavaHome():
     logging.info("Checking JAVA_HOME environment variable")
     try:
-        if platform.system() == "Windows":
-            ds = subprocess.run(["echo", "%JAVA_HOME%"], shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        else:
-            ds = subprocess.run(["echo", "$JAVA_HOME"], shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ds = subprocess.run(["echo", "$JAVA_HOME"], shell = True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if ds.returncode != 0:
             logging.error("Failed to check JAVA_HOME env")
             return
@@ -71,8 +101,8 @@ def checkJavaHome():
         if ds.stdout is None:
             logging.error("JAVA_HOME not set. Please set JAVA_HOME environment variable and try again.")
             return
-    except subprocess.CalledProcessError:
-        logging.error(subprocess.CalledProcessError.stderr)
+    except subprocess.CalledProcessError as e:
+        logging.error(e.output)
 
 
 def bytesToMB(v):
@@ -91,6 +121,12 @@ def checkConfigFile():
             exit(0)
         else:
             logging.info("please enter y/ n")
+
+
+def runPartnerServiceInfo():
+    logging.info("If you are running scenarios with id authentication. You have to start the partner service. Go to the ivv-orchestrator dir and run java -jar dependency_jars/authentication-partnerdemo-service.jar")
+    user_input = input("Hello contributor, If you are running scenarios with id authentication. You have to start the partner service . Go to the ivv-orchestrator dir and run java -jar dependency_jars/authentication-partnerdemo-service.jar. Then press any key to continue...")
+
 
 
 def copydir(source, dest):
@@ -133,8 +169,10 @@ def reporthook(blocknum, blocksize, totalsize):
 def copyDependencies():
     src = os.path.join(rootPath, "dependencies")
     dest = os.path.join(rootPath, "ivv-orchestrator", "local")
+    if not os.path.exists(dest):
+        os.makedirs(dest)
     logging.info("Coping files from "+src+" to "+dest)
-    copytree(os.path.join(rootPath, "dependencies"), os.path.join(rootPath, "ivv-orchestrator", "local"))
+    copy_tree(src, dest)
     logging.info("Coping files from "+src+" to "+dest+" done")
 
 
@@ -152,32 +190,52 @@ def downloadPartnerService():
     logging.info("Downloading Partner service from "+partnerDemoServiceUrl+" to "+partnerDemoServiceJarPath+" done")
 
 
-def downloadRegistrationService():
-    logging.info("Downloading Registration service from "+registrationServicesUrl+" to "+registrationServicesJarPath)
-    urllib.request.urlretrieve(registrationServicesUrl, registrationServicesJarPath, reporthook)
-    logging.info("Downloading Registration service from "+registrationServicesUrl+" to "+registrationServicesJarPath+" done")
+# def downloadRegistrationService():
+#     logging.info("Downloading Registration service from "+registrationServicesUrl+" to "+registrationServicesJarPath)
+#     urllib.request.urlretrieve(registrationServicesUrl, registrationServicesJarPath, reporthook)
+#     logging.info("Downloading Registration service from "+registrationServicesUrl+" to "+registrationServicesJarPath+" done")
 
+def downloadRegClient():
+    logging.info("Downloading Regclient from "+registrationClientUrl+" to "+registrationClientZipPath)
+    urllib.request.urlretrieve(registrationClientUrl, registrationClientZipPath, reporthook)
+    logging.info("Downloading Local database from "+registrationClientUrl+" to "+registrationClientZipPath+" done")
 
-def downloadLocalDatabase():
-    logging.info("Downloading Local database from "+databaseUrl+" to "+databaseLocalZipPath)
-    urllib.request.urlretrieve(databaseUrl, databaseLocalZipPath, reporthook)
-    logging.info("Downloading Local database from "+databaseUrl+" to "+databaseLocalZipPath+" done")
-    logging.info("Unzipping "+databaseLocalZipPath)
-    with zipfile.ZipFile(databaseLocalZipPath, 'r') as zip_ref:
-        zip_ref.extractall(databaseLocalFolderPath)
-    logging.info("Removing "+databaseLocalZipPath)
-    os.remove(databaseLocalZipPath)
-
+def extractLocalDatabase():
+    logging.info("Unzipping "+registrationClientZipPath)
+    with zipfile.ZipFile(registrationClientZipPath, 'r') as zip_ref:
+        zip_ref.extractall(os.path.join(rootPath, '.tmp'))
+    copy_tree(os.path.join(tmpPath, 'db'), os.path.join(rootPath, 'ivv-orchestrator', 'db'))
+    logging.info("Copied database to "+os.path.join(rootPath, 'ivv-orchestrator', 'db'))
 
 def downloadIdentySDK():
     logging.info("Downloading Partner service from "+identySDKUrl+" to "+identySDKJarPath)
     urllib.request.urlretrieve(identySDKUrl, identySDKJarPath, reporthook)
     logging.info("Downloading Partner service from "+identySDKUrl+" to "+identySDKJarPath+" done")
 
+def removeTempFolder():
+    shutil.rmtree(tmpPath)
+
+def removeLogsAndReport():
+    logging.info("Removing registration-services logs")
+    for file in os.listdir(os.path.join(rootPath, 'logs')):
+        if file.endswith(".log"):
+            os.remove(os.path.join(rootPath, 'logs', file))
+
+    logging.info("Removing testrig logs")
+    testRigLogs = os.path.join(rootPath, 'ivv-orchestrator', 'testRun', 'reports')
+    for file in os.listdir(testRigLogs):
+        if file.endswith(".log") or file.endswith(".html"):
+            os.remove(os.path.join(testRigLogs, file))
 
 def buildProject():
     try:
-        ds = subprocess.Popen(['mvn', 'clean', 'install', '-f', rootPath+'/pom.xml', '-Dmaven.test.skip=true'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cmdargs = []
+        cmdargs.append('-Dmaven.test.skip=true')
+        cmdargs.append('-Denvironment='+environment)
+        cmdargs.append('-DemailServerHost='+emailServerHost)
+        cmdargs.append('-DemailServerUsername='+emailServerUsername)
+        cmdargs.append('-DemailServerPassword='+emailServerPassword)
+        ds = subprocess.Popen(['mvn', 'clean', 'install', '-f', rootPath+'/pom.xml']+cmdargs, shell = True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         while True:
             output = ds.stdout.readline()
             if output == b'' and ds.poll() is not None:
@@ -185,14 +243,19 @@ def buildProject():
                 break
             if output:
                 logging.info(output.strip())
-    except subprocess.CalledProcessError:
-        logging.error(subprocess.CalledProcessError.stderr)
+    except subprocess.CalledProcessError as e:
+        logging.error(e.output)
         exit(1)
 
 
 def runTests():
     try:
-        ds = subprocess.Popen(['mvn', 'test', '-f', rootPath+'/pom.xml'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cmdargs = []
+        cmdargs.append('-Denvironment='+environment)
+        cmdargs.append('-DemailServerHost='+emailServerHost)
+        cmdargs.append('-DemailServerUsername='+emailServerUsername)
+        cmdargs.append('-DemailServerPassword='+emailServerPassword)
+        ds = subprocess.Popen(['mvn', 'test', '-f', rootPath+'/pom.xml']+cmdargs, shell = True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         logging.error(ds.stderr)
         while True:
             output = ds.stdout.readline()
@@ -201,35 +264,44 @@ def runTests():
                 return
             if output:
                 logging.info(output.strip())
-    except subprocess.CalledProcessError:
-        logging.error(subprocess.CalledProcessError.stderr)
+    except subprocess.CalledProcessError as e:
+        logging.error(e.output)
         exit(1)
 
 
 def setup():
+    preSetup()
     checkPlatform()
     checkMaven()
     checkJavaHome()
     copyDependencies()
     copyConfigFile()
     downloadPartnerService()
-    downloadRegistrationService()
-    downloadLocalDatabase()
     downloadIdentySDK()
+    downloadRegClient()
+    extractLocalDatabase()
+    removeTempFolder()
 
 
 def run():
+    preRun()
+    removeLogsAndReport()
     checkPlatform()
     checkMaven()
     checkJavaHome()
     checkConfigFile()
     buildProject()
+    runPartnerServiceInfo()
     runTests()
 
+def test():
+    downloadPartnerService()
 
 if args.mode == 'setup':
     setup()
 elif args.mode == 'run':
     run()
+elif args.mode == 'test':
+    test()
 else:
     logging.warning("subcommand not found")
